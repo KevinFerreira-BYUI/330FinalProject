7a3ed4aaa8f837fdff049ef53044630da0a48614
Kevin <kferreira@byupathway.edu>
2025-09-30 12:37:01 -0300

    going

---
+++ src/scripts/API-data.mjs
@@ -0,0 +1,62 @@
+const ApiUrl = import.meta.env.VITE_SERVER_URL;
+
+export async function getAllPokemons() {
+    try{
+        let response = await fetch(`${ApiUrl}pokemon`);
+    
+        if (!response.ok){
+            throw new Error(`Request error: ${response.status}`);
+        }
+
+        let pokeData = await response.json();
+        console.log(pokeData);
+
+        return pokeData.results;
+    } catch(error){
+        console.error(`Error: ${error}`);
+    }
+};
+
+export async function getPokemonByName(pokemonName) {
+    try{
+        let response = await fetch(`${ApiUrl}pokemon/${pokemonName}`);
+    
+        if (!response.ok){
+            throw new Error(`Request error: ${response.status}`);
+        }
+
+        let pokeData = await response.json();
+        console.log(pokeData);
+
+        return pokeData;
+    } catch(error){
+        console.error(`Error: ${error}`);
+    }
+};
+
+export async function getPokemonById(pokemonId) {
+    try{
+        let response = await fetch(`${ApiUrl}pokemon/${pokemonId}`);
+    
+        if (!response.ok){
+            throw new Error(`Request error: ${response.status}`);
+        }
+
+        let pokeData = await response.json();
+        console.log(pokeData);
+
+        return pokeData;
+    } catch(error){
+        console.error(`Error: ${error}`);
+    }
+};
+
+export async function getPokeDetail(url) {
+    const response = await fetch(url);
+    const pokeData = await response.json();
+    return {
+        name: pokeData.name,
+        type: pokeData.types.map(pType => pType.type.name).join(", "),
+        img: pokeData.sprites.other['official-artwork'].front_default
+    };
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 import '../styles/style.css'
+import { getAllPokemons, getPokemonByName, getPokemonById, getPokeDetail } from './API-data.mjs'
+
+async function loadPokeDataTemplate() {
+    let pokemons = await getAllPokemons();
+    const template = document.querySelector(".poke-card-template");
+    const container = document.getElementById("poke-section");
+
+    for (const pokemon of pokemons){
+        const details = await getPokeDetail(pokemon.url);
+        // console.log(details.name);
+        // console.log(details.type);
+        // console.log(details.img);
+
+        const clone = template.content.cloneNode(true);
+        const [img, pName, pType] = clone.querySelectorAll("img, #poke-name, #poke-type");
+
+        img.src = `${details.img}`;
+        img.alt = `${details.name}`;
+        pName.textContent = `${details.name}`;
+        pType.textContent = `${details.type}`;
+
+        container.appendChild(clone);
+    };
+};
+
+loadPokeDataTemplate();
 
 
